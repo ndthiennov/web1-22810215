90fde3bc707e1e2b627f5ce51108cb3c1be97a67
ndthiennov <ndthiennov@gmail.com>
2025-05-13 09:51:51 +0700

    Contact send email

--- js/22810215.js
+++ js/22810215.js
@@ -1,4 +1,5 @@
 const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users';
 
 async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
@@ -12,4 +13,20 @@ async function loadData(request, templateId, viewId) {
     // console.log(html);
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);
+}
+
+async function getAuthenticateToken(username, password){
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+    let result = await response.json();
+    if(response.status == 200){
+        return result.token;
+    }
+    throw new Error(result.message);
 }
